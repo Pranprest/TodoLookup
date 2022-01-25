838f8bbbcb64a6ca1f58c8bb40573c73e952ffc0
Pranprest <79233261+Pranprest@users.noreply.github.com>
2022-01-25 17:26:38 -0300

    added first implementation - WIP

--- src/todo.py
+++ src/todo.py
@@ -18,35 +18,52 @@ def argParser():
 
     _parser.add_argument("keyword",
                          type=str,
-                         help="keyword that will be searched instead of TODOs",
-                         default="TODO")
+                         nargs="?",
+                         help="keyword that will be searched instead of TODOs"
+                         )
+
+    _parser.add_argument("-b",
+                         "--bare",
+                         help="removes colors and line count",
+                         action="store_true",
+                         default=False,
+                         )
 
     return _parser
 
 
-def find_in_file(file_abs_path: Path, search_string: str, color: bool = True) -> None:
+def find_in_file(file_abs_path: Path, search_string: str, bare: bool = False) -> None:
+    # TODO: Make it possible to search more than one string at a time
     with open(file_abs_path, mode='rb') as f:
         with mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) as s:
             for index, line in enumerate(iter(s.readline, b"")):
                 line = line.decode("utf-8")
                 if line.find(search_string) != -1:
-                    if color == True:
+                    if bare == True:
+                        print(line, end="")
+                    else:
                         line = line.replace(
                             search_string, f"{Fore.BLUE}{search_string}{Fore.WHITE}")
-                    print(f"line {index}: {line}", end="")
+                        print(
+                            f"{Fore.RED}line {index}:{Fore.WHITE} {line}", end="")
 
 
 def main() -> None:
     args = argParser().parse_args()
     filepath = Path(os.path.abspath(args.file))
 
+    if args.keyword == None:
+        args.keyword == "TODO"
+
     if not filepath.exists():
         raise OSError("File does not exist.")
     if filepath.is_dir():
+        # TODO: make it possible to search things in a whole dir
         raise NotImplementedError(
             "This script still doesnt support directories!")
 
-    find_in_file(file_abs_path=filepath, search_string=args.keyword)
+    find_in_file(file_abs_path=filepath,
+                 search_string=args.keyword, bare=args.bare)
 
 
 if __name__ == '__main__':
