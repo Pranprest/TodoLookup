ef7859d4d1876341c752fb2d39996bc00464bf9a
Pranprest <79233261+Pranprest@users.noreply.github.com>
2022-01-25 18:11:12 -0300

    fixed todo not properly accepting STDIN

--- src/todo.py
+++ src/todo.py
@@ -15,9 +15,13 @@ def argParser():
 
     _parser.add_argument("file",
                          type=Path,
-                         help="file or directory that will be used for searching")
+                         nargs="?",
+                         default=(None if sys.stdin.isatty() else sys.stdin),
+                         help="file or directory that will be used for searching, if empty, stdin is used"
+                         )
 
-    _parser.add_argument("keyword",
+    _parser.add_argument("-s",
+                         "--keyword", "--search",
                          type=str,
                          nargs="?",
                          help="keyword that will be searched instead of TODOs"
@@ -49,26 +53,39 @@ def find_in_file(file_abs_path: Path, search_string: str, bare: bool = False) ->
                             f"{Fore.RED}line {index}:{Fore.WHITE} {line}", end="")
 
 
+def find_from_stdin(stdin, search_string: str, bare: bool = False) -> None:
+    for index, line in enumerate(sys.stdin):
+        if line.find(search_string) != -1:
+            if bare == True:
+                print(line, end="")
+            else:
+                line = line.replace(
+                    search_string, f"{Fore.BLUE}{search_string}{Fore.WHITE}")
+                print(
+                    f"{Fore.RED}line {index}:{Fore.WHITE} {line}", end="")
+
+
 def main() -> None:
-    if sys.argv[1]:
-        args = argParser().parse_args()
-    else:
-        find_from_stdin(sys.stdin)
-    filepath = Path(os.path.abspath(args.file))
+    args = argParser().parse_args()
 
     if args.keyword is None:
         args.keyword = "TODO"
 
+    if not args.file is str:
+        find_from_stdin(args.file, args.keyword)
+        sys.exit(0)
+
+    filepath = Path(os.path.abspath(args.file))
+
     if not filepath.exists():
         raise OSError("File does not exist.")
-
     if filepath.is_dir():
         # TODO: make it possible to search things in a whole dir
         raise NotImplementedError(
             "This script still doesnt support directories!")
-
-    find_in_file(file_abs_path=filepath,
-                 search_string=args.keyword, bare=args.bare)
+    else:
+        find_in_file(file_abs_path=filepath,
+                     search_string=args.keyword, bare=args.bare)
 
 
 if __name__ == '__main__':
