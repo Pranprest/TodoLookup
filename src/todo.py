3ce1d6f37b5e5297f4f1014e5bdb3cd6b3c42d7b
Pranprest <79233261+Pranprest@users.noreply.github.com>
2022-01-27 22:14:59 -0300

    feat: added searching in directories & config - config.toml added for storing allowed extensions - added cache files to gitignore - removed redundant code (imports)

--- src/todo.py
+++ src/todo.py
@@ -4,7 +4,7 @@ import os
 import sys
 import argparse
 from pathlib import Path
-from io import TextIOBase
+import toml
 # TODO: Make this an installable package
 
 currosslash = "\\" if (os.name == "nt") else "/"
@@ -40,6 +40,23 @@ def argParser():
     return _parser
 
 
+def get_extensions() -> set[str]:
+    currosslash = "\\" if (os.name == "nt") else "/"
+    cfg_file = Path(
+        f"{Path(__file__).parent.absolute()}{currosslash}config.toml")
+    with open(cfg_file, "r") as f:
+        cfg = toml.load(f)
+    return set(cfg["config"]["allowed_extensions"])
+
+
+def find_in_dir(dir: Path, search_list: list[str], allowed_extensions: set[str], bare: bool = False):
+    files_dir = [Path(x).absolute()
+                 for x in Path(dir).iterdir() if Path(x).is_file() and Path(x).suffix in allowed_extensions]
+    for file in files_dir:
+        print(f"Results in {file}:")
+        find_in_file(file, search_list=search_list, bare=bare)
+
+
 def find_in_file(file_abs_path: Path, search_list: list[str], bare: bool = False) -> None:
     with open(file_abs_path, mode='rb') as f:
         with mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) as s:
@@ -55,7 +72,7 @@ def find_in_file(file_abs_path: Path, search_list: list[str], bare: bool = False
                             f"{Fore.RED}line {index}:{Fore.WHITE} {line}", end="")
 
 
-def find_from_stdin(stdin, search_list: list[str], bare: bool = False) -> None:
+def find_from_stdin(search_list: list[str], bare: bool = False) -> None:
     for index, line in enumerate(sys.stdin):
         if any((search_string := x) in line for x in search_list):
             if bare == True:
@@ -71,12 +88,14 @@ def main() -> None:
     parser = argParser()
     args = parser.parse_args()
 
+    if len(args.keyword) <= 1:
+        args.keyword.append("TODO")
     if args.file == None:
         parser.print_help()
         sys.exit(0)
 
     if not sys.stdin.isatty():
-        find_from_stdin(args.file, args.keyword)
+        find_from_stdin(args.keyword, args.bare)
         sys.exit(0)
 
     filepath = Path(os.path.abspath(args.file))
@@ -85,9 +104,8 @@ def main() -> None:
         raise OSError("File does not exist.")
 
     if filepath.is_dir():
-        # TODO: make it possible to search things in a whole dir
-        raise NotImplementedError(
-            "This script still doesnt support directories!")
+        find_in_dir(dir=args.file, search_list=args.keyword,
+                    allowed_extensions=get_extensions(), bare=args.bare)
     else:
         find_in_file(file_abs_path=filepath,
                      search_list=args.keyword, bare=args.bare)
