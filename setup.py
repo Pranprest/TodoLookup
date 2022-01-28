ab849682d04485a77382741bc0521b3389b890e7
Pranprest <79233261+Pranprest@users.noreply.github.com>
2022-01-27 23:59:04 -0300

    fix: fixed broken package folder

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 if __name__ == '__main__':
     setup(
         entry_points={
-            'console_scripts': ['todolookup=todolookup.todolookup:main'],
+            'console_scripts': ['todo=todolookup.todo:main'],
         }
     )
