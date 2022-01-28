069cc110cf4a07ecbdde7c65c82b35a7056ca93e
Pran <79233261+Pranprest@users.noreply.github.com>
2022-01-28 00:07:54 -0300

    1st release and installable package (#1)
    * added first implementation - WIP
    
    * fixed TODO not being assigned as the default argument
    
    * todo accepts input from STDIN as arg
    
    * fixed todo not properly accepting STDIN
    
    * better way to figure out if stdin is going to be used or not
    
    * fixed exception when no file is inputed
    
    * feat: can now search multiples string in file
    - also removed redundant code (imports)!
    
    * feat: added searching in directories & config
    - config.toml added for storing allowed extensions
    - added cache files to gitignore
    - removed redundant code (imports)
    
    * added handling for extensions when searching in directories
    - add, list and remove dir options added
    
    * feat: made this whole project a installable package & changed name to TodoLookup
    
    * fix: fixed broken package folder
    
    * refactor: updated readme for handeling the project name change

--- README.md
+++ README.md
@@ -1,20 +1,32 @@
-# Todo Finder
+# Todo Lookup
 ## Overview
 Finds TODO(s) and other comment warnings in your project folder(s)
 - It's pretty much just a copy of GREP, but it searches for TODOs, FIXMEs when used on a file by default
 - It searches your whole directory for the keyword (on only non-excluded extensions)
 
 ```
-usage: todo.py [-h] file keyword
+usage: todo [-h] [-s [KEYWORD ...]] [-b] [--add-ext [EXTENSION ...]] [--list-ext] [--remove-ext [EXTENSION ...]]
+            [file]
 
 Find TODO(s), FIXME(s) or whatever your are searching
 
 positional arguments:
-  file        file or directory that will be used for searching
-  keyword     keyword that will be searched instead of TODOs   
+  file                  file or directory that will be used for searching, if empty, stdin is used
 
 options:
-  -h, --help  show this help message and exit
+  -h, --help            show this help message and exit
+  -s [KEYWORD ...], --keyword [KEYWORD ...], --search [KEYWORD ...]
+                        keywords that will be searched instead of TODOs
+  -b, --bare            removes colors and line count
+
+extension management:
+  ways to manage which extensions are looked after when directories are inputed
+
+  --add-ext [EXTENSION ...]
+                        adds an extension to the list of allowed extensions
+  --list-ext            list all allowed extensions
+  --remove-ext [EXTENSION ...]
+                        remove an extension to the list of allowed extensions
 ```
 
 ## Note
---
+++ src/todolookup/todo.py
@@ -0,0 +1,188 @@
+import mmap
+from xml.dom import NotFoundErr
+from colorama import Fore
+import os
+import sys
+import argparse
+from pathlib import Path
+import toml
+
+currosslash = "\\" if (os.name == "nt") else "/"
+currfilepath = f"{Path(__file__).parent.absolute()}{currosslash}"
+cfg_file = Path(f"{Path(__file__).parent.absolute()}{currosslash}config.toml")
+
+
+def argParser():
+    _parser = argparse.ArgumentParser(
+        description='Find TODO(s), FIXME(s) or whatever your are searching')
+
+    _parser.add_argument("file",
+                         type=Path,
+                         nargs="?",
+                         default=(None if sys.stdin.isatty() else sys.stdin),
+                         help="file or directory that will be used for searching, if empty, stdin is used"
+                         )
+
+    _parser.add_argument("-s",
+                         "--keyword", "--search",
+                         type=str,
+                         nargs="*",
+                         default=["TODO", "FIXME"],
+                         help="keywords that will be searched instead of TODOs"
+                         )
+
+    _parser.add_argument("-b",
+                         "--bare",
+                         help="removes colors and line count",
+                         action="store_true",
+                         default=False,
+                         )
+
+    ext_group = _parser.add_argument_group(
+        "extension management", "ways to manage which extensions are looked after when directories are inputed")
+
+    ext_group.add_argument("--add-ext",
+                           help="adds an extension to the list of allowed extensions",
+                           nargs="*",
+                           metavar="EXTENSION")
+
+    ext_group.add_argument("--list-ext",
+                           help="list all allowed extensions",
+                           action="store_true",
+                           )
+
+    ext_group.add_argument("--remove-ext",
+                           help="remove an extension to the list of allowed extensions",
+                           nargs="*",
+                           metavar="EXTENSION")
+    return _parser
+
+
+def get_extensions() -> set[str]:
+    with open(cfg_file, "r") as f:
+        cfg = toml.load(f)
+
+    if not "config" in cfg:
+        raise NotFoundErr("Config key not found in config file")
+
+    if len(cfg.get("allowed_extensions")) <= 0:
+        raise NotFoundErr(
+            "No allowed extensions were added, add one with --add-ext!")
+
+    return set(cfg["config"]["allowed_extensions"])
+
+
+def find_in_dir(dir: Path, search_list: list[str], allowed_extensions: set[str], bare: bool = False):
+    files_dir = [Path(x).absolute()
+                 for x in Path(dir).iterdir() if Path(x).is_file() and Path(x).suffix in allowed_extensions]
+    for file in files_dir:
+        print(f"Results in {file}:")
+        find_in_file(file, search_list=search_list, bare=bare)
+
+
+def find_in_file(file_abs_path: Path, search_list: list[str], bare: bool = False) -> None:
+    with open(file_abs_path, mode='rb') as f:
+        with mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) as s:
+            for index, line in enumerate(iter(s.readline, b"")):
+                line = line.decode("utf-8")
+                if any((search_string := x) in line for x in search_list):
+                    if bare == True:
+                        print(line, end="")
+                    else:
+                        line = line.replace(
+                            search_string, f"{Fore.BLUE}{search_string}{Fore.WHITE}")
+                        print(
+                            f"{Fore.RED}line {index}:{Fore.WHITE} {line}", end="")
+
+
+def find_from_stdin(search_list: list[str], bare: bool = False) -> None:
+    for index, line in enumerate(sys.stdin):
+        if any((search_string := x) in line for x in search_list):
+            if bare == True:
+                print(line, end="")
+            else:
+                line = line.replace(
+                    search_string, f"{Fore.BLUE}{search_string}{Fore.WHITE}")
+                print(
+                    f"{Fore.RED}line {index}:{Fore.WHITE} {line}", end="")
+
+
+def main() -> None:
+    parser = argParser()
+    args = parser.parse_args()
+
+    if not cfg_file.exists() or os.stat(cfg_file).st_size == 0:
+        with open(cfg_file, "w") as f:
+            pog = {"config": {"allowed_extensions": []}}
+            f.seek(0)
+            toml.dump(pog, f)
+            f.truncate()
+    with open(cfg_file, "r") as f:
+        cfg = toml.load(f)
+
+    if args.add_ext != None and len(args.add_ext) >= 1:
+        args.add_ext = set(args.add_ext)
+        for ext in args.add_ext:
+            ext.strip()
+            if ext[0] != ".":
+                ext = "." + ext
+            if ext in cfg["config"]["allowed_extensions"]:
+                print(
+                    f"Extension {ext} is already on the list of allowed extensions, skipping.")
+            else:
+                print(f"Extension {ext} added")
+                cfg["config"]["allowed_extensions"].append(ext)
+        with open(cfg_file, "w") as f:
+            f.seek(0)
+            toml.dump(cfg, f)
+            f.truncate()
+        sys.exit(0)
+
+    if args.list_ext == True:
+        print(f"These are the current allowed extensions:")
+        print(", ".join(cfg["config"]["allowed_extensions"]))
+        sys.exit(0)
+
+    if args.remove_ext != None and len(args.remove_ext) >= 1:
+        for ext in args.remove_ext:
+            ext.strip()
+            if ext[0] != ".":
+                ext = "." + ext
+            if ext not in cfg["config"]["allowed_extensions"]:
+                print(
+                    f"Extension {ext} is not in the list of allowed extensions, skipping.")
+            else:
+                cfg["config"]["allowed_extensions"].remove(ext)
+                print(f"Extension {ext} removed sucessfully")
+        with open(cfg_file, "w") as f:
+            f.seek(0)
+            toml.dump(cfg, f)
+            f.truncate()
+        sys.exit(0)
+
+    if len(args.keyword) <= 1:
+        args.keyword.append("TODO")
+
+    if args.file == None:
+        parser.print_help()
+        sys.exit(0)
+
+    if not sys.stdin.isatty():
+        find_from_stdin(args.keyword, args.bare)
+        sys.exit(0)
+
+    filepath = Path(os.path.abspath(args.file))
+
+    if not filepath.exists():
+        raise OSError("File does not exist.")
+
+    if filepath.is_dir():
+        find_in_dir(dir=args.file, search_list=args.keyword,
+                    allowed_extensions=get_extensions(), bare=args.bare)
+    else:
+        find_in_file(file_abs_path=filepath,
+                     search_list=args.keyword, bare=args.bare)
+
+
+if __name__ == '__main__':
+    main()
